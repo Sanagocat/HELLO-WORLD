30de9dcb00c477f5f67be2c6ad07cea3efcd03db
hryvnia <>
2022-05-18 11:23:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+print("Hello World!!")
+print("Hi There~?")
+print("halo")
+
+P = 1
+a = 0
+b = 2
+c = "test"
+Pabcabaaaacbababap = "HAHAHAHA"
+
+print ("Hello" + " World")
+print (" if you press one q, then this shuts down.")
+programEnd = True
+while(programEnd): #repeat forever
+  gradeNumber = input("Input your Grade (Press q to quit):")
+  if gradeNumber == "1" :
+    print ("you is "+str(gradeNumber) + "th"+" grade" )
+  elif gradeNumber == "2" :
+    print ("you is "+str(gradeNumber) + "th"+" grade" )
+  elif gradeNumber == "3" :
+    print ("you is "+str(gradeNumber) + "th"+" grade" )
+  elif gradeNumber == "4" :
+    print ("you is "+str(gradeNumber) + "th"+" grade" )
+  elif gradeNumber == "5" :
+    print ("you is "+str(gradeNumber) + "th"+" grade" )
+  elif gradeNumber == "6" :
+    print ("you is "+str(gradeNumber) + "th"+" grade" )
+  elif gradeNumber == "7" :
+    print ("you are "+str(gradeNumber) + "th"+" grade" )
+  elif gradeNumber == "8" :
+    print ("you are "+str(gradeNumber) + "th"+" grade" )
+  elif gradeNumber == "9" :
+    print ("you are "+str(gradeNumber) + "th"+" grade" )
+  elif gradeNumber == "10" :
+    print ("you are "+str(gradeNumber) + "th"+" grade" )
+  elif gradeNumber == "q":
+    programEnd=False
+    print ("bye bye!")
+  else:
+    print (" that is not a number")
+    
+
+#Homework
+#Input User Name, and Print User'setattr
+#"Your Name is JBSong!!"
+
+
+
+
+
+
+
+
+
+
